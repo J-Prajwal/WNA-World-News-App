c8060bcb5d5e29211f2faae161451808ebe93905
J-Prajwal <luckyjais058@gmail.com>
2023-01-29 12:11:22 +0530

    setup for WNA ready with navbar

---
+++ src/Routes/MainRoutes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Homepage from './Homepage'
+
+const MainRoutes = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/' element={<Homepage />} />
+        <Route path='/' element={<Homepage />} />
+        <Route path='/' element={<Homepage />} />
+        <Route path='/' element={<Homepage />} />
+    </Routes>
+  )
+}
+
+export default MainRoutes
