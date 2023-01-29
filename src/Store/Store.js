e35a4418a43da30a577be6ecd27eef287a4fcbc9
J-Prajwal <luckyjais058@gmail.com>
2023-01-29 12:42:02 +0530

    setup: store setup ready to use & implement

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,12 +1,18 @@
-import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
+import {
+  applyMiddleware,
+  combineReducers,
+  compose,
+  legacy_createStore,
+} from "redux";
 import { authReducer } from "./Auth/auth.reducer";
 import { newsReducer } from "./News/news.reducer";
 import thunk from "redux-thunk";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
-    auth: authReducer,
-    news: newsReducer
-})
+  auth: authReducer,
+  news: newsReducer,
+});
 
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
+export const store = legacy_createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
